228684a6939a80ce0492b142c3ca1b719bc0288f
Sebastian Diaz <jsebasdc@gmail.com>
2018-10-02 10:37:59 -0500

    Inicio Proyecto Landing Asus

---
+++ src/index.js
@@ -0,0 +1,3 @@
+require('./styles.scss');
+
+const test = []; 
---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'main.js'
+    },
+    module: {
+        rules: [{
+            test: /\.scss$/,
+            use: [
+                MiniCssExtractPlugin.loader, // creates style nodes from JS strings
+                "css-loader", // translates CSS into CommonJS
+                "sass-loader" // compiles Sass to CSS, using Node Sass by default
+            ]
+        }]
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: 'styles.css',
+            chunkFilename: 'id.css'
+        })
+    ]
+};
